a0886bfc8f1fb8fa03178dcee663c6771e376461
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-09-30 13:53:35 +0530

    Create User schema

---
+++ models/user.ts
@@ -0,0 +1,21 @@
+import mongoose from "mongoose";
+
+const schema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name is required."],
+  },
+  email: {
+    type: String,
+    required: [true, "Email is required."],
+    unique: [true, "Email must be unique."],
+  },
+  password: {
+    type: String,
+    required: [true, "Password is required."],
+    select: true,
+    minLength: [3, "Password must be at least 3 characters."],
+  },
+});
+
+export const User = mongoose.model("User", schema);
