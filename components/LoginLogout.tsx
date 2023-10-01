36a94f0357e136e702917d9c67a24a12cb00fa0f
Shubham <77922893+shubhamxdd@users.noreply.github.com>
2023-10-01 13:41:22 +0530

    Logout users on click of button

--- components/LoginLogout.tsx
+++ components/LoginLogout.tsx
@@ -4,10 +4,31 @@ import Link from "next/link";
 import React, { useContext } from "react";
 import Button from "./Button";
 import { MyContext } from "@/context/MyContext";
+import toast from "react-hot-toast";
+import { useRouter } from "next/navigation";
 
 const LoginLogout = () => {
-  const { user } = useContext(MyContext);
-  return user.id ? <Button text="Logout" /> : <Link href="/login">Login</Link>;
+  const { user, setUser } = useContext(MyContext);
+  const router = useRouter();
+  const logoutHandler = async () => {
+    try {
+      const res = await fetch("/api/auth/logout");
+      const data = await res.json();
+      console.log("logging out");
+      router.push("/login");
+      setUser({});
+
+      if (!data.user._id) return toast.error(data.message);
+      toast.success(data.message);
+    } catch (error) {
+      // return toast.error(data.message);
+    }
+  };
+  return user._id ? (
+    <button onClick={() => logoutHandler()}>Logout</button>
+  ) : (
+    <Link href="/login">Login</Link>
+  );
 };
 
 export default LoginLogout;
